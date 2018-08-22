4a41e8b35e81325bd7a37727a31b89ab250a1b45
Ryooooooga <ryoga_314@yahoo.co.jp>
2018-08-22 23:54:09 +0900

    Update

--- bfx64.hpp
+++ bfx64.hpp
@@ -37,42 +37,92 @@ namespace bfx64
 	public:
 		explicit AssemblyGenerator()
 			: m_stream()
+			, m_label(0)
 		{
 			m_stream
-				<< "    .global  main"          << '\n'
-				<< "    .section .text"         << '\n'
-				<< "main:"                      << '\n'
-				<< "    push  %rbp"             << '\n'
-				<< "    mov   %rsp,     %rbp"   << '\n'
-				<< "    sub   $0xffff,  %rsp"   << '\n'
-				<< "    lea   1(%rbp),  %rdi"   << '\n'
-				<< "    movb  $0,       (%rdi)" << '\n'
-				;
+				<< "    .global  main"              << '\n'
+				<< "    .section .text"             << '\n'
+				<< "main:"                          << '\n'
+				<< "    push  %rbp"                 << '\n'
+				<< "    mov   %rsp,     %rbp"       << '\n'
+				<< "    sub   $0xffff,  %rsp"       << '\n'
+				<< "    mov   %rsp,     %rdi"       << '\n'
+				<< ".LX:"                           << '\n'
+				<< "    movq  $0,       (%rdi)"     << '\n'
+				<< "    add   $8,       %rdi"       << '\n'
+				<< "    cmp   %rbp,     %rdi"       << '\n'
+				<< "    jg    .LX"                  << '\n'
+				<< "    mov   %rsp,     %rdi"       << '\n';
 		}
 
 		void finish()
 		{
 			m_stream
-				<< "    movzb (%rdi),   %rax"   << '\n'
-				<< "    mov   %rbp,     %rsp"   << '\n'
-				<< "    pop   %rbp"             << '\n'
-				<< "    ret"                    << '\n';
+				<< "    movzb (%rdi),   %rax"       << '\n'
+				<< "    mov   %rbp,     %rsp"       << '\n'
+				<< "    pop   %rbp"                 << '\n'
+				<< "    ret"                        << '\n';
+		}
+
+		void emit_next()
+		{
+			m_stream
+				<< "    add   $1,       %rdi"       << '\n';
+		}
+
+		void emit_prev()
+		{
+			m_stream
+				<< "    sub   $1,       %rdi"       << '\n';
 		}
 
 		void emit_increment()
 		{
 			m_stream
-				<< "    movzb (%rdi),   %rax"   << '\n'
-				<< "    add   $1,       %rax"   << '\n'
-				<< "    movb  %al,      (%rdi)" << '\n';
+				<< "    movzb (%rdi),   %rax"       << '\n'
+				<< "    add   $1,       %rax"       << '\n'
+				<< "    movb  %al,      (%rdi)"     << '\n';
 		}
 
 		void emit_decrement()
 		{
 			m_stream
-				<< "    movzb (%rdi),   %rax"   << '\n'
-				<< "    sub   $1,       %rax"   << '\n'
-				<< "    movb  %al,      (%rdi)" << '\n';
+				<< "    movzb (%rdi),   %rax"     << '\n'
+				<< "    sub   $1,       %rax"     << '\n'
+				<< "    movb  %al,      (%rdi)"   << '\n';
+		}
+
+		void emit_loop()
+		{
+			Loop loop =
+			{
+				m_label++,
+				m_label++,
+			};
+
+			m_loops.emplace_back(loop);
+
+			m_stream
+				<< ".L" << loop.top << ":"        << '\n'
+				<< "    movzb (%rdi),   %rax"     << '\n'
+				<< "    cmp   $0,       %rax"     << '\n'
+				<< "    je    .L" << loop.end     << '\n';
+		}
+
+		void emit_loop_end()
+		{
+			if (m_loops.empty())
+			{
+				throw std::out_of_range {"invalid loop"};
+			}
+
+			const auto loop = m_loops.back();
+
+			m_loops.pop_back();
+
+			m_stream
+				<< "    jmp   .L" << loop.top     << '\n'
+				<< ".L" << loop.end << ":"        << '\n';
 		}
 
 		// Uncopyable, unmovable.
@@ -89,29 +139,49 @@ namespace bfx64
 		}
 
 	private:
-		std::string m_entry;
+		struct Loop
+		{
+			std::size_t top;
+			std::size_t end;
+		};
+
 		std::ostringstream m_stream;
+		std::vector<Loop> m_loops;
+		std::size_t m_label;
 	};
 
 	template <typename CodeGenerator>
 	[[nodiscard]]
 	auto compile(std::string_view source)
 	{
-		std::vector<std::size_t> loops;
-		loops.reserve(std::count(std::begin(source), std::end(source), '['));
-
 		CodeGenerator gen;
 
 		for (const auto c : source)
 		{
 			switch (c)
 			{
+				case '<':
+					gen.emit_prev();
+					break;
+
+				case '>':
+					gen.emit_next();
+					break;
+
 				case '+':
 					gen.emit_increment();
 					break;
 
 				case '-':
 					gen.emit_decrement();
+					break;
+
+				case '[':
+					gen.emit_loop();
+					break;
+
+				case ']':
+					gen.emit_loop_end();
 					break;
 
 				default:
